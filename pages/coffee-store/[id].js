5894811804341481b863ce7b07677c4cf5ceefa3
Eramood <122992858+Eramood@users.noreply.github.com>
2023-06-23 20:41:11 +0200

    Get Static Props and Get Static Paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,26 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import coffeeStoresData  from '../../data/coffee-stores.json'
 
-const CoffeeStore = () =>{
+export function getStaticProps(staticProps){
+   const params = staticProps.params
+   return{
+      props:{
+         coffeeStore : coffeeStoresData.find((coffeeStore)=>{
+            return coffeeStore.id.toString() === params.id ;
+         })
+      }
+   }
+}
+
+export function getStaticPaths() {
+   return {
+     paths: [{ params: { id: "0" } }, { params: { id: "1" } }, { params: { id: "2" } }],
+     fallback: false
+   };
+ }
+
+const CoffeeStore = (props) =>{
    const router= useRouter();
    const query = router.query.id;
    return (
@@ -9,6 +28,8 @@ const CoffeeStore = () =>{
       <Link href="/">
          click here to go Home
       </Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
       </div>
    )
 };
