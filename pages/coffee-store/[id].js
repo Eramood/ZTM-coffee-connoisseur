46ce078ce25fe280226dac48981ba0c5bfbc8e2a
Eramood <122992858+Eramood@users.noreply.github.com>
2023-07-10 16:11:51 +0200

    fix fallback: true

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -15,14 +15,17 @@ export function getStaticProps(staticProps){
 
 export function getStaticPaths() {
    return {
-     paths: [{ params: { id: "0" } }, { params: { id: "1" } }, { params: { id: "2" } }],
-     fallback: false
+     paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+     fallback: true
    };
  }
 
 const CoffeeStore = (props) =>{
    const router= useRouter();
    const query = router.query.id;
+   if(router.isFallback){
+      return<div>Loading....</div>
+   }
    return (
       <div>Coffee shoppp is {query},
       <Link href="/">
