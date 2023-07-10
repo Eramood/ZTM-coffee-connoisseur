d80668659eb71cd4cf34a90c5b78de3034ce7cf2
Eramood <122992858+Eramood@users.noreply.github.com>
2023-07-10 17:20:38 +0200

    adding style to coffee store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,6 +1,10 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 import coffeeStoresData  from '../../data/coffee-stores.json'
+import styles from "../../styles/coffee-store.module.css"
+import Image from "next/image";
+import cls from "classnames";
 
 export function getStaticProps(staticProps){
    const params = staticProps.params
@@ -14,8 +18,15 @@ export function getStaticProps(staticProps){
 }
 
 export function getStaticPaths() {
+   const paths = coffeeStoresData.map((coffeeStore) => {
+      return {
+        params: {
+          id: coffeeStore.id.toString(),
+        },
+      };
+    });
    return {
-     paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+     paths,
      fallback: true
    };
  }
@@ -23,17 +34,57 @@ export function getStaticPaths() {
 const CoffeeStore = (props) =>{
    const router= useRouter();
    const query = router.query.id;
+   
    if(router.isFallback){
       return<div>Loading....</div>
    }
+   
+   const {address, name, neighbourhood, imgUrl } = props.coffeeStore;
+   const handleUpvoteButton = () => {};
+   
    return (
-      <div>Coffee shoppp is {query},
-      <Link href="/">
-         click here to go Home
-      </Link>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+   <div className={styles.layout}>
+      <Head>
+         <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <p>Back to home</p>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/places.svg" width="24" height="24" />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width="24" height="24" />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
       </div>
+   </div>
    )
 };
 export default CoffeeStore;
